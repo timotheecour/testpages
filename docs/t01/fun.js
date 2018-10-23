5fb28e3c126de04e8a994284034ae18afa331a5c
Timothee Cour <timothee.cour2@gmail.com>
2018-10-23 13:40:11 -0700

    _

--- docs/t01/fun.js
+++ docs/t01/fun.js
@@ -1,23 +1,22 @@
-console.log(dat);
+// console.log(dat);
 
 /*
 has been blocked by CORS policy: Cross origin requests are only supported for protocol schemes: http, data, chrome, chrome-extension, https.
 */
-/*
+
 function loadJSON(callback) {   
   var xobj = new XMLHttpRequest();
   xobj.overrideMimeType("application/json");
   // xobj.open('GET', '../news_data.json', true);
-  xobj.open('GET', 'data.json', true);
+  xobj.open('GET', 'data2.json', true);
   xobj.onreadystatechange = function () {
     if (xobj.readyState == 4 && xobj.status == "200") {
       callback(JSON.parse(xobj.responseText));
     }
   };
-  xobj.send(null);  
+  xobj.send(null);
 }
 
 loadJSON(function(json) {
-  console.log(json); // this will log out the json object
+  console.log(json);
 });
-*/
